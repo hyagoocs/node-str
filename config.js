c5cbb3799211bbacf46eceba2ccc08a1a4142853
Hyago Silva <hyago.silva@u4crypto.com>
2021-02-01 20:40:23 -0300

    automovel working

--- config.js
+++ config.js
@@ -1,3 +1,14 @@
-module.exports = {
-    connectionString: 'mongodb+srv://crudseidor:crudseidor@crudseidor.zzlhq.mongodb.net/crudseidor?retryWrites=true&w=majority'
-}
+const bluebird = require('bluebird');
+const mongoose = require('mongoose');
+mongoose.Promise = bluebird;
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://crudseidor:crudseidor@crudseidor.zzlhq.mongodb.net/crudseidor?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+module.exports = { mongoose }
--- src/models/automovel.js
+++ src/models/automovel.js
@@ -1,6 +1,6 @@
 'use-strict'
 
-const mongoose = require('mongoose');
+const { mongoose } = require('../../config');
 const Schema = mongoose.Schema;
 
 const schema = new Schema({
